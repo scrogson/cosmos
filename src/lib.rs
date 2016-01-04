c180829caef0868d5fe34d4a0d61fcfe5d060b50
Sonny Scroggin <scrogson@gmail.com>
2016-01-03 22:11:19 -0600

    frame allocator

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,70 @@
 #![feature(unique)]
 #![no_std]
 
+extern crate multiboot2;
 extern crate rlibc;
 extern crate spin;
 
 #[macro_use]
 mod vga_buffer;
+mod memory;
 
 #[no_mangle]
-pub extern fn rust_main() {
+pub extern fn rust_main(multiboot_info_addr: usize) {
     vga_buffer::clear_screen();
-    println!("Hello world{}", "!");
+
+    let boot_info = unsafe { multiboot2::load(multiboot_info_addr) };
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+    let elf_sections_tag = boot_info.elf_sections_tag().expect("Elf-sections tag required");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+    }
+
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}", section.addr, section.size, section.flags);
+    }
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
+    let multiboot_start = multiboot_info_addr;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("kernel start: 0x{:x}, kernel end: 0x{:x}",
+             kernel_start,
+             kernel_end);
+    println!("multiboot start: 0x{:x}, multiboot end: 0x{:x}",
+             multiboot_start,
+             multiboot_end);
+
+    let mut frame_allocator = memory::AreaFrameAllocator::new(
+        kernel_start as usize,
+        kernel_end as usize,
+        multiboot_start,
+        multiboot_end,
+        memory_map_tag.memory_areas()
+    );
+
+    for i in 0.. {
+        use memory::FrameAllocator;
+        if let None = frame_allocator.allocate_frame() {
+            println!("allocated {} frames", i);
+            break;
+        }
+    }
 
     loop {}
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
+#[lang = "eh_personality"]
+extern fn eh_personality() {
+}
+
+#[lang = "panic_fmt"]
+extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop {}
+}
